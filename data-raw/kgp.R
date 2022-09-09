f6b3583caf83b3cd36b16dab2e6511392e98955a
Stephen Turner <vustephen@gmail.com>
2022-09-09 05:44:32 -0400

    first commit

---
+++ data-raw/kgp.R
@@ -0,0 +1,6 @@
+## code to prepare `kgp` dataset goes here
+
+# https://github.com/igsr/1000Genomes_data_indexes
+# http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/
+
+usethis::use_data(kgp, overwrite = TRUE)
